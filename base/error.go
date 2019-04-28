84d9df9592efb5a32da9eae197d6161c5ba7b3f6
Demo <vimliang@gmail.com>
2019-04-28 23:39:59 +0800

    base

--- base/error.go
+++ base/error.go
@@ -4,39 +4,39 @@ import "fmt"
 
 type ErrorCode int
 
-const(
+const (
 	FGEBase = 10000
 )
 
 const (
-	Failed ErrorCode = -1
+	Failed  ErrorCode = -1
 	Success ErrorCode = 200
 )
 
-const(
-	EInternalError ErrorCode = 5 * FGEBase+iota
+const (
+	EInternalError ErrorCode = 5*FGEBase + iota
 )
 
 const (
-	ErrClientError ErrorCode = 4*FGEBase+iota
+	ErrClientError ErrorCode = 4*FGEBase + iota
 )
 
 var evtDesc = map[ErrorCode]string{
-	Failed:"Failed",
-	Success:"Success",
+	Failed:  "Failed",
+	Success: "Success",
 
-	EInternalError:"系统内部错误",
+	EInternalError: "系统内部错误",
 
-	ErrClientError:"客户端错误",
+	ErrClientError: "客户端错误",
 }
 
-func (e ErrorCode)String()string{
-	if value,ok:=evtDesc[e];ok{
+func (e ErrorCode) String() string {
+	if value, ok := evtDesc[e]; ok {
 		return value
 	}
 	return "Unknow"
 }
 
-func (e ErrorCode)Error()string{
-	return fmt.Sprintf("%d:%s",int(e),e.String())
+func (e ErrorCode) Error() string {
+	return fmt.Sprintf("%d:%s", int(e), e.String())
 }
