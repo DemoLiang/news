3f981cb850b614255fe8a3cb029fe00bff061c37
Demo <vimliang@gmail.com>
2019-04-28 23:36:34 +0800

    add config

--- config/config.go
+++ config/config.go
@@ -1,30 +1,67 @@
 package config
 
 import (
-	"os"
-
-	"github.com/spf13/viper"
+	"encoding/json"
+	"github.com/toolkits/file"
+	"news/base"
+	"sync"
 )
 
-// Viper viper
-var Config *viper.Viper
-var rootDir string
+var (
+	config  *GlobalConfig
+	cfglock sync.RWMutex
+)
 
-func env() {
-	Config = viper.New()
-	Config.SetConfigName("config")
-	Config.AddConfigPath(rootDir)
-	err := Config.ReadInConfig()
-	if err != nil {
-		panic("read config error")
-	}
+func Cfg() *GlobalConfig {
+	cfglock.Lock()
+	defer cfglock.Unlock()
+	return config
 }
 
-func init() {
-	var err error
-	rootDir, err = os.Getwd()
-	if err != nil {
-		panic("init config exception, because os.Getwd() failure")
+func ParserConfig(cfg string) {
+	if cfg == "" {
+		panic("use -c to specify configuration file")
+		return
 	}
-	env()
+	if !file.IsExist(cfg) {
+		panic("config file is not existent")
+		return
+	}
+	configContent, err := file.ToTrimString(cfg)
+	if err != nil {
+		panic("read config file error:" + err.Error())
+		return
+	}
+	var c GlobalConfig
+	err = json.Unmarshal([]byte(configContent), &c)
+	if err != nil {
+		panic("parser config file error " + err.Error())
+		return
+	}
+	cfglock.Lock()
+	defer cfglock.Unlock()
+	config = &c
+	base.Log("read config file:%v successfully", cfg)
+	return
+}
+
+type GlobalConfig struct {
+	Splider struct {
+		Parallelism int  `json:"parallelism"`
+		Delay       int  `json:"delay"`
+		All         bool `json:"all"`
+	} `json:"splider"`
+	Wechat struct {
+		Appid   string `json:"appid"`
+		Seceret string `json:"seceret"`
+		Token string `json:"token"`
+	} `json:"wechat"`
+	Mysql struct {
+		Addr         string `json:"addr"`
+		MaxIdleConns int    `json:"max_idle_conns"`
+		MaxOpenConns int    `json:"max_open_conns"`
+	} `json:"mysql"`
+	Http struct{
+		Listen string `json:"listen"`
+	} `json:"http"`
 }
