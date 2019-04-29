5ab210e206227c4d7107b7111a9861d006313bfa
Demo <vimliang@gmail.com>
2019-04-30 01:17:48 +0800

    update main

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"news/splider"
+	"flag"
+	"news/config"
+	"news/http"
+	"news/wechat"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func main(){
-	splider.Run()
+func main() {
+	cfg := flag.String("c", "./cfg.json", "cfg file")
+	flag.Parse()
+	config.ParserConfig(*cfg)
+
+	wechat.InitWeChatToken()
+	go wechat.UpdateWeChatToken()
+
+	//go cron.AutoFetch()
+	//go cron.AutoPublish()
+
+	go http.Start()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigs
+		os.Exit(0)
+	}()
+	select {}
 }
